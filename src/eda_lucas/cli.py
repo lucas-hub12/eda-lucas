89ea8dce01ae279402ba01302b60ff16e0e45b02
lucas_hub12 <hyeonggyunbae@gmail.com>
2025-01-21 16:51:36 +0900

    테스트 코드 True 확인

--- src/eda_lucas/cli.py
+++ src/eda_lucas/cli.py
@@ -2,24 +2,24 @@ from president_speech.db.parquet_interpreter import read_parquet, get_parquet_fu
 import pandas as pd
 import typer
 
-def group_by_count(keyword: str, ascending: bool = False):
+def group_by_count(keyword: str, asc: bool=True, rcnt: int=12):
     # todo:ascending, 출력 rows size
     # pytest 코드를 만들어보세요
     # import this <- 해석해보세요
     data_path = get_parquet_full_path()
     df = pd.read_parquet(data_path)
-    f_df = df[df['speech_text'].str.contains(keyword, case=False)]
-    rdf = f_df.groupby("president").size().reset_index(name="count")
-    rdf.sort_values(by='count', ascending=False).reset_index(drop=True)
-    sdf = rdf.sort_values(by='count', ascending=False).reset_index(drop=True)
-    print(sdf.to_string(index=False))
-    print(f"Rows size: {sdf.shape[0]}")
+    fdf = df[df['speech_text'].str.contains(keyword, case=False)]
+    gdf = fdf.groupby("president").size().reset_index(name="count")
+    sdf= gdf.sort_values(by='count', ascending=asc).reset_index(drop=True)
+    rdf = sdf.head(rcnt)
+    return rdf
+
+def print_group_by_count(keyword: str, asc: bool=True, rcnt: int=12):
+    rdf = group_by_count(keyword, asc, rcnt)
+    print(rdf.to_string(index=False))
+
 
 def entry_point():
-    typer.run(group_by_count)
+    typer.run(print_group_by_count)
 
-def test_addition():
-    assert 1 + 1 == 2
 
-def test_subtraction():
-    assert 5 - 3 == 2
